4a8128f1c541b5442f1824afeeb60ec70e101252
Ayanda Matiringe <acm@Ayandas-MacBook-Pro.local>
2019-07-30 18:16:11 +0200

    initial commit boilerplate for signin-app

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//initialize firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyDvduyJYVMZ0F0ONo3bvgOa0JSsDjFVEy8",
+  authDomain: "signin-app-f8676.firebaseapp.com",
+  databaseURL: "https://signin-app-f8676.firebaseio.com",
+  projectId: "signin-app-f8676",
+  storageBucket: "",
+  messagingSenderId: "905052368233",
+  appId: "1:905052368233:web:34647cff0769a86f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
