1f1e4edd9cb68e2fa89bd4ab00cbae4849d20737
Ayanda Matiringe <acm@Ayandas-MacBook-Pro.local>
2019-07-31 18:33:03 +0200

    deletes functions folder, implements mongodb/mongoose connection

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://amatirin:ayami92@signin-app-cluster-rbm02.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server/app.js
+++ server/app.js
@@ -2,11 +2,20 @@
   const express = require("express");
   const url = require("url");
   const path = require("path");
-  let publicPath = path.resolve(__dirname, '../dist');
-
-  const expressLayouts = require("express-ejs-layouts");
+  const mongoose = require("mongoose");
+  let publicPath = path.resolve(__dirname, "../dist");
   const PORT = process.env.PORT || 5000;
+  const expressLayouts = require("express-ejs-layouts");
+
   const app = express();
+
+  //DB connection
+  const db = require("../config/keys").MongoURI;
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
+
   //Routing
   app.use("/", require("../routes/index"));
   app.use("/users", require("../routes/users"));
@@ -14,7 +23,7 @@
   //EJS layouts
   app.use(express.static(publicPath));
   app.use(expressLayouts);
-  app.engine('html', require('ejs').renderFile);
+  app.engine("html", require("ejs").renderFile);
   app.set("view engine", "ejs");
 
   app.listen(PORT, console.log(`Server is running on port ${PORT}`));
